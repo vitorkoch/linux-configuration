598c111f179e1448691d5e74dbacd831578e18f3
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-08-12 18:45:59 -0300

    git.sh added

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# To-Do
+
+- [x] Git configuration
+- [x] Add WallPapers
+- [x] Add Fonts
+- [ ] Add WhiteSur theme
+- [ ] Add Candy Icons theme
+- [ ] Add Oreo Cursors
---
+++ git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Git configuration"
+read -p "Name: " name
+read -p "Email: " email
+
+git config --global user.name "$name"
+git config --global user.email "$email"
+git config --global credential.helper store
