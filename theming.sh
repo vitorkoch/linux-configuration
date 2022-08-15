2f881e220f299bcdff481807a54ee4bcd9515978
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-08-15 12:26:16 -0300

    update

--- theming.sh
+++ theming.sh
@@ -11,41 +11,34 @@ SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 #echo "Script dir: $SCRIPT_DIR"
 # ==========================================
 
-
 mkdir ~/theming
 cd ~/theming
+
 git clone https://github.com/EliverLara/candy-icons.git || {
     echo "Updating candy-icons"
     cd candy-icons ; git pull ; cd ~/theming
 }
-
 mkdir ~/.icons
-
 cp -r candy-icons ~/.icons
-
 
 git clone https://github.com/vitorkoch/Vimix-cursors.git || {
     echo "Updating Vimix-cursors"
     cd Vimix-cursors ; git pull ; cd ~/theming
 }
-
 Vimix-cursors/install.sh
-
 
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git || {
     echo "Updating WhiteSur-gtk-theme"
     cd WhiteSur-gtk-theme ; git pull ; cd ~/theming
 }
-
-WhiteSur-gtk-theme/install.sh -c Dark --normal -i simple
-
+WhiteSur-gtk-theme/install.sh -c Dark --normal -i simple -t purple -o normal
 
 mkdir ~/.fonts
 cp $SCRIPT_DIR/fonts/* ~/.fonts  
 
 # Tweaks
-gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark"
-gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-Dark"
+gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark-purple"
+gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-Dark-purple"
 gsettings set org.gnome.desktop.interface icon-theme "candy-icons"
 gsettings set org.gnome.desktop.interface cursor-theme "Vimix-cursors"
 gsettings set org.gnome.desktop.interface cursor-size 32
