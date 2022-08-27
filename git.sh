65917182b85a450c41c94bffe34a5d15c69b1021
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-08-27 12:08:24 -0300

    update

--- git.sh
+++ git.sh
@@ -8,5 +8,8 @@ git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global credential.helper store
 git config --global init.defaultBranch main
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global diff.colorMoved zebra
 
 echo "Sucessful configuration!"
