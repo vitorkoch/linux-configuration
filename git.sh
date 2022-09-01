e86111e0b2d6d7b71d64dc1e8f0b9b46e6f4a1cd
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-09-01 14:36:53 -0300

    add new git configurations

--- git.sh
+++ git.sh
@@ -7,7 +7,6 @@ read -p "Email: " email
 git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global credential.helper store
-git config --global init.defaultBranch main
 git config --global pull.rebase true
 git config --global fetch.prune true
 git config --global diff.colorMoved zebra
