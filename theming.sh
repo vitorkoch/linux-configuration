a4991bfa862346d4442d25732568354d15945f52
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-10-12 12:37:51 -0300

    add grub theming

--- theming.sh
+++ theming.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 # Source: https://stackoverflow.com/a/246128
 SOURCE=${BASH_SOURCE[0]}
@@ -14,29 +14,36 @@ SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 sudo echo "Sudo mode"
 mkdir ~/theming ; cd ~/theming
 
-# Shell
+echo "Shell"
 git clone https://github.com/vinceliuice/Orchis-theme.git 
 cd Orchis-theme ; sudo ./install.sh --libadwaita --size standard --theme default --color dark ; cd ~/theming
 
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 cd WhiteSur-gtk-theme ; sudo ./install.sh --color Dark --normalshowapps --icon simple -l --opacity normal --round --nord ; cd ~/theming
 
-# Icons
+echo "Icons"
 git clone https://github.com/vinceliuice/Tela-icon-theme.git
 cd Tela-icon-theme ; sudo ./install.sh ; cd ~/theming
 
 git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git
 cd papirus-icon-theme ; sudo ./install.sh ; cd ~/theming
 
-# Cursor
+echo "Cursor"
 cd ~/theming ; wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.2/Bibata-Modern-Classic.tar.gz
 tar -xf Bibata-Modern-Classic.tar.gz 
 sudo cp -r ./Bibata-Modern-Classic/ /usr/share/icons
 
-# Fonts
-mkdir ~/.fonts ; sudo cp $SCRIPT_DIR/fonts/* ~/.fonts
+echo "Grub"
+git clone https://github.com/vinceliuice/grub2-themes.git
+cd grub2-themes ; sudo ./install.sh --theme whitesur --icon whitesur ; cd ~/theming
 
-#rm -rf ~/theming
+echo "Fonts"
+sudo cp $SCRIPT_DIR/fonts/* /usr/share/fonts
+
+echo "Wallpapers"
+sudo cp $SCRIPT_DIR/wallpapers/* /usr/share/backgrounds
+
+rm -rf ~/theming
 echo -e "
 \033[1m
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -46,6 +53,9 @@ Installed
 - Icons: Tela & Papirus
 - Shell: WhiteSur-Nord & Orchis
 - Fonts: Cascadia Code, Arial, Times New Roman, JetBrains Mono
+- Some Wallpapers
+- Whitesur grub theme
+
 Go to your system settings (GNOME Tweaks in GNOME Shell) 
 and apply the changes
 
